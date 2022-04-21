f6827f6f446b67e64a25c5ecf7311a1fdf515fb5
fukurodami <fukurodami@gmail.com>
2022-04-21 16:44:35 +0700

    second version

--- lib/views.py
+++ lib/views.py
@@ -8,6 +8,12 @@ class BooksView(View):
     """Список книг"""
     def get(self, request):
         books = Book.objects.all()
+        record_take = RecordReader.objects.filter(existence=True)
+        for i in books:
+            for j in record_take:
+                if i.id == j.book.id:
+                    i.stock -= 1
+                    
         return render(request, "books.html", {"books_list": books})
 
 
